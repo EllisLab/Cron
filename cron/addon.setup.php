052fb5b3ecddd846023dbb6d8af7ab8ae3487d63
Robin Sowell <robin.sowell@ellislab.com>
2015-07-16 23:01:05 -0400

    ExpressionEngine 3.x compatible

---
+++ cron/addon.setup.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+	'author'      => 'EllisLab',
+	'author_url'  => 'https://ellislab.com/',
+	'name'        => 'Cron - Base Plugin',
+	'description' => 'Allows the regular, scheduled calling of plugins and modules',
+	'version'     => '2.0',
+	'namespace'   => 'User\Addons\Cron',
+	'settings_exist' => FALSE
+);
